64fbbf45ca663df8269d520bf27dd9909cf25f54
Maritza <maritzaesparza05@gmail.com>
2015-11-08 15:58:22 -0600

    Add social auth

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social.apps.django_app.default',
     'registration',
     'store',
 )
@@ -65,6 +66,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social.apps.django_app.context_processors.backends',
+                'social.apps.django_app.context_processors.login_redirect',
+
             ],
         },
     },
@@ -72,6 +76,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bookstore.wsgi.application'
 
+AUTHENTICATION_BACKENDS = {
+    'social.backends.facebook.FacebookOAuth2',
+    'django.contrib.auth.backends.ModelBackend'
+}
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
@@ -120,3 +128,7 @@ EMAIL_HOST_PASSWORD = 'pistacheLinux1114'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'books@mysterybooks.com'
+
+# Social Auth = Facebook
+SOCIAL_AUTH_FACEBOOK_KEY = '928421487243344'
+SOCIAL_AUTH_FACEBOOK_SECRET = '104308a600ec5be6f0811d41b044b26f'
--- bookstore/urls.py
+++ bookstore/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
+    url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^store/', include('store.urls'), name='store'),
 
 ]
